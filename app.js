26e5f11a2aed43a80e5a394b73a5deb210dcb6df
DominicFung <fung_dominic@hotmail.com>
2017-06-24 17:17:35 -0400

    adding postgres

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var pg = require('pg').Pool;
+
+var pool = new pg({
+    user: 'Dominic',
+    password: 'CV1234',
+    host: 'localhost',
+    database: 'ChatDB',
+    max: 10,
+    idleTimeoutMillis: 1000
+});
+
+pool.on('error', function(e, client){
+    console.log(e);
+});
+
+var query = pool.query('SELECT * FROM Student');
+// query.on("row", function (row, result){
+//     result.addRow(row);
+// });
+// query.on("end", function(result){
+//     console.log(JSON.stringify(result.rows,null, "   "));
+// });
+
 var app = express();
+var _exPORT = 3001;
 //var server = require('http').createServer(app);
 
 var ws = require("nodejs-websocket");
-var _PORT = 3000;
+var _wsPORT = 3000;
 
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
@@ -23,11 +47,11 @@ var server = ws.createServer(function (conn){
     conn.on("close", function (code, reason){
         console.log("closed: "+code);
     });
-}).listen(_PORT);
+}).listen(_wsPORT);
 
-console.log("websocket is running on port: " + _PORT);
+console.log("websocket is running on port: " + _wsPORT);
 
-app.listen(_PORT+1);
+app.listen(_exPORT);
 
-console.log("http interface is running on port: " + (_PORT+1));
+console.log("http interface is running on port: " + (_exPORT));
 
