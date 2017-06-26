bed5c8ae4a93ea8c11a5a2cd043b9f8b12e9de0c
DominicFung <fung_dominic@hotmail.com>
2017-06-25 21:51:37 -0400

    trying out sequelize instead of pg

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
-var express = require('express');
-var pg = require('pg').Pool;
+var express = require("express");
+var Sequelize = require('sequelize');
+// var pg = require('pg');
 
-var pool = new pg({
-    user: 'Dominic',
-    password: 'CV1234',
-    host: 'localhost',
-    database: 'ChatDB',
-    max: 10,
-    idleTimeoutMillis: 1000
+// var config = {
+//     user: 'postgres',
+//     database: 'ChatDB',
+//     password: 'CV1234',
+//     host: 'localhost',
+//     port: '5432',
+//     max: 10,
+//     idleTimeoutMillis: 30000
+// };
+
+// var client = new pg.Client(config);
+
+// client.on('error', function(err,client){
+//     console.error('idel client error',err.message, err.stack);
+// });
+
+// client.connect();
+
+// client.query("INSERT INTO School (school_name) values($1)", ['Waterloo']);
+// //client.query("UPDATE student set first_name = 'Dom2' WHERE last_name = 'Fung'");
+// //var query = client.query("SELECT * FROM Student");
+
+// //console.log(query);
+// client.end();
+
+const sequelize = new Sequelize("ChatDB", "postgres", "CV1234", {
+  host: "localhost",
+  dialect: "postgres",
+  pool: { max: 5, min: 0, idle: 10000 }
 });
 
-pool.on('error', function(e, client){
-    console.log(e);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-var query = pool.query('SELECT * FROM Student');
-// query.on("row", function (row, result){
-//     result.addRow(row);
-// });
-// query.on("end", function(result){
-//     console.log(JSON.stringify(result.rows,null, "   "));
-// });
+//const Student = sequelize.
 
 var app = express();
 var _exPORT = 3001;
@@ -29,29 +51,29 @@ var _exPORT = 3001;
 var ws = require("nodejs-websocket");
 var _wsPORT = 3000;
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
-
-app.post('/login', jsonParser, function(){
-    console.log(req.body);
-    //respond with only information??
+app.post("/login", jsonParser, function() {
+  console.log(req.body);
+  //respond with only information??
 });
 
-var server = ws.createServer(function (conn){
+var server = ws
+  .createServer(function(conn) {
     console.log("new connection");
-    conn.on("text", function (str){
-        console.log("Received: "+str);
-        conn.sendText(str.toUpperCase()+"!");
+    conn.on("text", function(str) {
+      console.log("Received: " + str);
+      conn.sendText(str.toUpperCase() + "!");
     });
-    conn.on("close", function (code, reason){
-        console.log("closed: "+code);
+    conn.on("close", function(code, reason) {
+      console.log("closed: " + code);
     });
-}).listen(_wsPORT);
+  })
+  .listen(_wsPORT);
 
 console.log("websocket is running on port: " + _wsPORT);
 
 app.listen(_exPORT);
 
-console.log("http interface is running on port: " + (_exPORT));
-
+console.log("http interface is running on port: " + _exPORT);
